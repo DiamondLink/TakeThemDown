20a01a55c907d64e4adadbf3964234a086934b10
DiamondLink <adressepourgithub@gmail.com>
2020-04-08 13:27:11 +0200

    finished port scanner. some changes has to be done

--- Scan/PortScanner/PortScanner.py
+++ Scan/PortScanner/PortScanner.py
@@ -1,4 +1,4 @@
-# This script runs on Python 3
+# Port Scanner by DiamondLink
 import socket, threading, sys, os, time
 sys.path.append(os.path.join(sys.path[0],'..','..','baseFunctions'))
 from functions import *
@@ -11,9 +11,9 @@ class SinglePortScan(threading.Thread):
     
     def run(self):
 
-        if protocol == "TCP":
+        if self.protocol == "TCP":
             self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        elif protocol == "UDP":
+        elif self.protocol == "UDP":     #NOT WORKING RN
             self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #THIS NEED TO BE FIXED
         #self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) idrk what this does
         self.sock.settimeout(self.timeout)
@@ -24,22 +24,22 @@ class SinglePortScan(threading.Thread):
         except socket.timeout:
             pass
 
+        self.sock.close()
+
 def PortScanner(ip : str,portRange : list,protocol : str,threadPerSecond : int,timeout : float):#set threadPerSecond to 0 if you don't want a thread limit
     """scan ips for open ports. Port range must a list with start port and end port. Setting threadPerSecond to 0 won't do a thread limit"""
 
     openPorts = list()
     portScanThreads = list()
-    for ports in range(portRange[0],portRange[1]):
-        portScanThreads.append(SinglePortScan(ip,ports,timeout))
 
-    for threads in portScanThreads:
-        threads.start()
+    for ports in range(portRange[0],portRange[1]):
+        portScanThreads.append(SinglePortScan(ip,ports,protocol,timeout))
+        portScanThreads[ports - portRange[0]].start()
         if threadPerSecond != 0:
             time.sleep(1/threadPerSecond)
 
     for threads in portScanThreads:
         threads.join()
-
 
     for threads in portScanThreads:
         if threads.isOpen:
@@ -55,5 +55,5 @@ if __name__=="__main__":
         port[i] = int(port[i])
 
     threadPerSecond = int(input("Thread per second : "))
-    timeout = float(input("timeout :"))
-    print(PortScanner(ip,port,threadPerSecond,timeout))
+    timeout = float(input("timeout : "))
+    print(PortScanner(ip,port,"TCP",threadPerSecond,timeout))
